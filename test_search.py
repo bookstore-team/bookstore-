84c3284b1c0f15385f83e13a957381faca383103
xxxxxiwu <90242145+xxxxxiwu@users.noreply.github.com>
2022-12-05 15:11:21 +0800

    更改测试

---
+++ test_search.py
@@ -0,0 +1,28 @@
+import time
+import pytest
+from fe.access import search
+from fe import conf
+
+
+class TestSearch:
+    @pytest.fixture(autouse=True)
+    def pre_run_initialization(self):
+        # do before test
+        # 例子
+        self.mysearch = search.Search(conf.URL)
+        yield
+        # do after test
+
+    def test_param_ok(self):
+        code = self.mysearch.param_search(title=self.title, author=self.author, tags=self.tags)
+        assert code == 200
+
+        code = self.mysearch.param_search(title=self.title, author=self.author, tags=self.tags, store_id=self.store_id)
+        assert code == 200
+
+    def test_content_ok(self):
+        code = self.mysearch.content_search(sub_content=self.subcontent)
+        assert code == 200
+
+        code = self.mysearch.content_search(sub_content=self.subcontent, store_id=self.store_id)
+        assert code == 200
