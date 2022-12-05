d712925d8eba9cdbc16abba934c68ff1037ee2c7
xxxxxiwu <90242145+xxxxxiwu@users.noreply.github.com>
2022-12-05 15:17:22 +0800

    好像没push成功

--- fe/test/test_search.py
+++ fe/test/test_search.py
@@ -7,11 +7,9 @@ from fe import conf
 class TestSearch:
     @pytest.fixture(autouse=True)
     def pre_run_initialization(self):
-        # do before test
         # 例子
         self.mysearch = search.Search(conf.URL)
         yield
-        # do after test
 
     def test_param_ok(self):
         code = self.mysearch.param_search(title=self.title, author=self.author, tags=self.tags)
@@ -26,3 +24,4 @@ class TestSearch:
 
         code = self.mysearch.content_search(sub_content=self.subcontent, store_id=self.store_id)
         assert code == 200
+        # 
